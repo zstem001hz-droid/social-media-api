73b87667d7b88f0bc2942528aa8f2b87535c6daf
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-15 19:14:55 -0400

    chore: scaffold server structure with section comments

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // Dependencies
 // Load environment variables from .env
 require("dotenv").config();
-
 const express = require("express");
 const { MongoClient } = require("mongodb");
+
+// App Configuration
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+
+// Databases
+
+// Routes
+
+// Error Handling
+
+// Server - Port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
