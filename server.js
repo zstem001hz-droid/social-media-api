9514bbf0fc12ac9e11f407e6115a21f5cdede2fe
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-15 18:53:01 -0400

    feat: add express app instance and server configuration

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ require("dotenv").config();
 
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const app = express();
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
